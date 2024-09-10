2effcfa466e96eef65b580205b7b3b7d89659844
Pol Alvarez <pol.avms@gmail.com>
2024-09-10 13:14:43 +0200

    requests: refactored to use only sync code

--- l402/client/requests.py
+++ l402/client/requests.py
@@ -1,8 +1,44 @@
 import requests
-import asyncio
 from l402.client import Client
-
 from .exceptions import RequestException
+from .preimage_provider import PreimageProvider
+from .credentials import CredentialsService, parse_http_402_response
+
+class Client(Client):
+    def __init__(self, preimage_provider: PreimageProvider = None, 
+                 credentials_service: CredentialsService = None):
+        super().__init__(preimage_provider, credentials_service)
+
+    def _add_authorization_header(self, kwargs, credentials):
+        """Adds the L402 Authorization header to the request."""
+        headers = kwargs.setdefault('headers', {})
+        headers['Authorization'] = credentials.authentication_header()
+
+    def _handle_402_payment_required(self, url: str, response: requests.Response) -> CredentialsService:
+        """Handles a 402 Payment Required response."""
+        creds = parse_http_402_response(response)
+        creds.set_location(url)
+        preimage = self.preimage_provider.get_preimage(creds.invoice)
+        if not preimage:
+            raise Exception("Payment failed.")
+
+        creds.preimage = preimage
+        self.credentials_service.store(creds)
+        return creds
+
+    def request(self, method: str, url: str, **kwargs) -> requests.Response:
+        creds = self.credentials_service.get(url)
+        if creds:
+            self._add_authorization_header(kwargs, creds)
+
+        with requests.Session() as session:
+            response = session.request(method, url, **kwargs)
+            if response.status_code != 402:
+                return response
+
+            new_creds = self._handle_402_payment_required(url, response)
+            self._add_authorization_header(kwargs, new_creds)
+            return session.request(method, url, **kwargs)
 
 class Session:
     _instance = None
@@ -19,17 +55,16 @@ class Session:
         """Check if the request client is configured."""
         return self._configured
 
-    def configure(self, preimage_provider: str = None, credentials_service: str = None) -> None:
+    def configure(self, preimage_provider: PreimageProvider = None, credentials_service: CredentialsService = None) -> None:
         """Configure the request client with given providers and services."""
         self._client = Client(preimage_provider, credentials_service)
-        self._client.configure(preimage_provider, credentials_service)
         self._configured = True
 
     def request(self, method: str, url: str, **kwargs) -> requests.Response:
         """Perform a request with optional parameters."""
         if not self.is_configured:
             raise RequestException("No request client configured.")
-        return asyncio.run(self._client.request(method, url, **kwargs))
+        return self._client.request(method, url, **kwargs)
 
     def get(self, url: str, **kwargs) -> requests.Response:
         """Perform a GET request with optional parameters."""
